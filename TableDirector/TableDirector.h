b9fcb7571322bf454c75a20eb90fd67037ff9b39
Dibadgo <3temal@gmail.com>
2019-10-22 15:34:35 +0300

    Initial Commit

---
+++ TableDirector/TableDirector.h
@@ -0,0 +1,19 @@
+//
+//  TableDirector.h
+//  TableDirector
+//
+//  Created by Artyom on 22.10.2019.
+//  Copyright © 2019 Artyom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TableDirector.
+FOUNDATION_EXPORT double TableDirectorVersionNumber;
+
+//! Project version string for TableDirector.
+FOUNDATION_EXPORT const unsigned char TableDirectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableDirector/PublicHeader.h>
+
+
